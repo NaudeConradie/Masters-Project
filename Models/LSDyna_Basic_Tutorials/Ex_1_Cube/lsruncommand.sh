aa184bb38bf924035c37801df10b57c217de33a9
Naudé Conradie <19673418@sun.ac.za>
2019-11-18 08:26:57 +0200

    Update

---
+++ Models/LSDyna_Basic_Tutorials/Ex_1_Cube/lsruncommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/home/naude/Programs/LSTC/LSPREPOST/lsrun" -exp "/home/naude/Programs/LSTC/LSDYNA/ls-dyna_smp_s_r1010_x64_redhat5_ifort160 i=/home/naude/Documents/Masters-Project/Models/LSDyna_Basic_Tutorials/Ex_1_Cube/Cube.k ncpu=2 memory=20m" -submit
