08a64a8780c334bc72bc16d275f8e9b1e8a223f6
Naudé Conradie <19673418@sun.ac.za>
2019-11-20 14:50:39 +0200

    Update

---
+++ Models/LSDyna_Unit_Cell_Corner/lsruncommand.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"/home/naude/Programs/LSTC/LSPREPOST/lsrun" -exp "/home/naude/Programs/LSTC/LSDYNA/ls-dyna_smp_s_r1010_x64_redhat5_ifort160 i=/home/naude/Documents/Masters-Project/Models/LSDyna_Unit_Cell_Corner/Unit_Cell_Corner.k ncpu=2 memory=20m" -submit
