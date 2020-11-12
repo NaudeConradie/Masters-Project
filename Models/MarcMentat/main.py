e6acc10db7d5d23fb0743601923c99451b342c5d
NaudeConradie <19673418@sun.ac.za>
2020-11-12 07:44:00 +0200

    commit

--- Models/MarcMentat/main.py
+++ Models/MarcMentat/main.py
@@ -51,12 +51,12 @@ def main():
     p_mag = 0.025
 
     #   The unit generation method
-    g_meth = "c"
+    g_meth = "l"
     #   The analysis method
     a_meth = "g"
 
     #   Genetic algorithm parameters
-    gen = 10
+    gen = 3
     prob = [0.5, 0.1, 0.5]
     point = [1, 2, 2]
 
